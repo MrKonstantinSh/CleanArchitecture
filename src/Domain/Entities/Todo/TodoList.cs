83fcf346b03c0d39915bd49d4d2410054bc43d09
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-12-31 13:15:01 +0300

    feat(domain): add colour property for Todo lists

--- src/Domain/Entities/Todo/TodoList.cs
+++ src/Domain/Entities/Todo/TodoList.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using CleanArchitecture.Domain.ValueObjects;
 
 namespace CleanArchitecture.Domain.Entities.Todo
 {
     public class TodoList : BaseEntity
     {
         public string Title { get; set; }
+
+        public Colour Colour { get; set; } = Colour.White;
 
         public IList<TodoItem> Items { get; set; }
     }
