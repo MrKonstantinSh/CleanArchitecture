e65dcad26edc5ec414e4165ab830c091c7f6ef4f
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-12-31 12:44:32 +0300

    feat(domain): add priority levels to Todo items

--- src/Domain/Entities/Todo/TodoItem.cs
+++ src/Domain/Entities/Todo/TodoItem.cs
@@ -1,10 +1,14 @@
-﻿namespace CleanArchitecture.Domain.Entities.Todo
+﻿using CleanArchitecture.Domain.Enums;
+
+namespace CleanArchitecture.Domain.Entities.Todo
  {
      public class TodoItem : BaseEntity
      {
          public string Title { get; set; }
  
          public string Note { get; set; }
+
+         public PriorityLevel Priority { get; set; }
  
          public TodoList List { get; set; }
      }
