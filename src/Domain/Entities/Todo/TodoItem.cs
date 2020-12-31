03a1c8832e6def0abc76124e44bb0a6c53838cac
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-12-31 12:39:57 +0300

    feat(domain): add Todo entities

---
+++ src/Domain/Entities/Todo/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Domain.Entities.Todo
+ {
+     public class TodoItem : BaseEntity
+     {
+         public string Title { get; set; }
+ 
+         public string Note { get; set; }
+ 
+         public TodoList List { get; set; }
+     }
+ }
---
+++ src/Domain/Entities/Todo/TodoList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities.Todo
+{
+    public class TodoList : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public IList<TodoItem> Items { get; set; }
+    }
+}
