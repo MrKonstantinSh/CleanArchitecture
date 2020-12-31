c842eddc56ff141ed0a0f84da4cfabda9863cd77
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-12-31 15:58:16 +0300

    feat(app): add the IAppDbContext interface

---
+++ src/Application/Common/Interfaces/IAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities.Todo;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Common.Interfaces
+{
+    public interface IAppDbContext
+    {
+        DbSet<TodoList> TodoLists { get; set; }
+
+        DbSet<TodoItem> TodoItems { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
