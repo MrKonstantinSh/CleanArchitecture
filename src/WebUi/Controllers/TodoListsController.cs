c806e75c22f4892f81d399f5de4b5a021bc5c31d
Shulga Konstantin <mrkonstantinsh@gmail.com>
2021-01-01 17:50:07 +0300

    feat(web): implement getting todo lists

---
+++ src/WebUi/Controllers/TodoListsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using CleanArchitecture.Application.Todos.TodoLists.Queries.GetTodoLists;
+using CleanArchitecture.Application.Todos.Vms;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUi.Controllers
+{
+    public class TodoListsController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<TodosVm>> Get()
+        {
+            return await Mediator.Send(new GetTodoListsQuery());
+        }
+    }
+}
