dc746004bc8113a7d0674c917cd04f3703870b46
Shulga Konstantin <mrkonstantinsh@gmail.com>
2021-01-01 17:48:00 +0300

    feat(app): implement getting todo lists

---
+++ src/Application/Todos/Dtos/TodoListDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CleanArchitecture.Application.Common.Mappings;
+using CleanArchitecture.Domain.Entities.Todo;
+
+namespace CleanArchitecture.Application.Todos.Dtos
+{
+    public class TodoListDto : IMapFrom<TodoList>
+    {
+        public TodoListDto()
+        {
+            Items = new List<TodoItemDto>();
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Colour { get; set; }
+
+        public IList<TodoItemDto> Items { get; set; }
+    }
+}
---
+++ src/Application/Todos/TodoLists/Queries/GetTodoLists/GetTodoListsQuery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.Todos.Dtos;
+using CleanArchitecture.Application.Todos.Vms;
+using CleanArchitecture.Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Todos.TodoLists.Queries.GetTodoLists
+{
+    public class GetTodoListsQuery : IRequest<TodosVm>
+    {
+    }
+
+    public class GetTodosQueryHandler : IRequestHandler<GetTodoListsQuery, TodosVm>
+    {
+        private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetTodosQueryHandler(IAppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<TodosVm> Handle(GetTodoListsQuery request, CancellationToken cancellationToken)
+        {
+            return new TodosVm
+            {
+                PriorityLevels = Enum.GetValues(typeof(PriorityLevel))
+                    .Cast<PriorityLevel>()
+                    .Select(p => new PriorityLevelDto {Value = (int) p, Name = p.ToString()})
+                    .ToList(),
+
+                Lists = await _context.TodoLists
+                    .AsNoTracking()
+                    .ProjectTo<TodoListDto>(_mapper.ConfigurationProvider)
+                    .OrderBy(t => t.Title)
+                    .ToListAsync(cancellationToken)
+            };
+        }
+    }
+}
