14c6ac85061225b79031228a921e24a884ed2027
Shulga Konstantin <mrkonstantinsh@gmail.com>
2021-01-01 21:58:17 +0300

    feat(app): implement todo list update

---
+++ src/Application/Todos/TodoLists/Commands/UpdateTodoList/UpdateTodoListCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Exceptions;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Domain.Entities.Todo;
+using MediatR;
+
+namespace CleanArchitecture.Application.Todos.TodoLists.Commands.UpdateTodoList
+{
+    public class UpdateTodoListCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+    
+    public class UpdateTodoListCommandHandler : IRequestHandler<UpdateTodoListCommand>
+    {
+        private readonly IAppDbContext _context;
+
+        public UpdateTodoListCommandHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateTodoListCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.TodoLists.FindAsync(request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TodoList), request.Id);
+            }
+
+            entity.Title = request.Title;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Application/Todos/TodoLists/Commands/UpdateTodoList/UpdateTodoListCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Todos.TodoLists.Commands.UpdateTodoList
+{
+    public class UpdateTodoListCommandValidator : AbstractValidator<UpdateTodoListCommand>
+    {
+        private readonly IAppDbContext _context;
+
+        public UpdateTodoListCommandValidator(IAppDbContext context)
+        {
+            _context = context;
+
+            RuleFor(v => v.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(200).WithMessage("Title must not exceed 200 characters.")
+                .MustAsync(BeUniqueTitle).WithMessage("The specified title already exists.");
+        }
+        
+        private async Task<bool> BeUniqueTitle(UpdateTodoListCommand model, string title,
+            CancellationToken cancellationToken)
+        {
+            return await _context.TodoLists
+                .Where(l => l.Id != model.Id)
+                .AllAsync(l => l.Title != title, cancellationToken: cancellationToken);
+        }
+    }
+}
