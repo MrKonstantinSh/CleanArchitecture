73f2f03fe24e207c697f618c40652f468e40b745
Shulga Konstantin <mrkonstantinsh@gmail.com>
2021-01-01 16:12:57 +0300

    feat(app): implement UnhandledExceptionBehaviour

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using CleanArchitecture.Application.Behaviours;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +10,8 @@ namespace CleanArchitecture.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
 
             return services;
         }
