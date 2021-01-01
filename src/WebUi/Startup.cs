382dbf099726271ab37b483e7965af4d6e8494a9
Shulga Konstantin <mrkonstantinsh@gmail.com>
2021-01-01 18:09:14 +0300

    feat: implement custom error handling

--- src/WebUi/Startup.cs
+++ src/WebUi/Startup.cs
@@ -1,9 +1,12 @@
 using CleanArchitecture.Application;
 using CleanArchitecture.Application.Common.Interfaces;
 using CleanArchitecture.Infrastructure;
+using CleanArchitecture.WebUi.Filters;
 using CleanArchitecture.WebUi.Services;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +33,14 @@ namespace CleanArchitecture.WebUi
 
             services.AddHttpContextAccessor();
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(options => 
+                options.Filters.Add<ApiExceptionFilterAttribute>()).AddFluentValidation();
+
+            // Customise default API behaviour
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
             
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
