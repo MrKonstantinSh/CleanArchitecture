02beab913daf8cd51cbee49eadd3c098fed78085
Shulga Konstantin <mrkonstantinsh@gmail.com>
2021-01-02 13:28:29 +0300

    feat(app): implement updating todo item

---
+++ src/Application/Todos/TodoItems/Commands/UpdateTodoItem/UpdateTodoItemCommand.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Exceptions;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Domain.Entities.Todo;
+using MediatR;
+
+namespace CleanArchitecture.Application.Todos.TodoItems.Commands.UpdateTodoItem
+{
+    public class UpdateTodoItemCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Done { get; set; }
+    }
+    
+    public class UpdateTodoItemCommandHandler : IRequestHandler<UpdateTodoItemCommand>
+    {
+        private readonly IAppDbContext _context;
+
+        public UpdateTodoItemCommandHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateTodoItemCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.TodoItems.FindAsync(request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TodoItem), request.Id);
+            }
+
+            entity.Title = request.Title;
+            entity.Done = request.Done;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
