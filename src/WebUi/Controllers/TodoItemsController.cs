57f1febd4f23dced3081c8a0ac08bc961ea00d34
Shulga Konstantin <mrkonstantinsh@gmail.com>
2021-01-02 12:10:41 +0300

    feat(web): implement getting a list of todo items

---
+++ src/WebUi/Controllers/TodoItemsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Models;
+using CleanArchitecture.Application.Todos.Dtos;
+using CleanArchitecture.Application.Todos.TodoItems.Queries.GetTodoItemsWithPagination;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUi.Controllers
+{
+    public class TodoItemsController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<PaginatedList<TodoItemDto>>> GetTodoItemsWithPagination(
+            [FromQuery] GetTodoItemsWithPaginationQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
